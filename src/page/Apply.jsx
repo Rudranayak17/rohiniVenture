0255e69fa76fd0c0948e4ffe9a1203add55bc066
Rudranayak17 <rudranayak311@gmail.com>
2024-11-11 15:19:06 +0530

    update the ui

--- src/page/Apply.jsx
+++ src/page/Apply.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Side from '../components/leftSide';
 import Form from '../components/FormDetail';
 
-
 const Apply = () => {
   return (
-
-    <div className="flex flex-col md:flex-row p-6 space-x-4 gap-7">
-      <Side />
-      <Form />
+    <div className="flex justify-center items-center p-6 gap-7 min-h-screen">
+  
+        <Form />
+  
     </div>
-
   );
 };
 
