1f27e43236a9ac9e4707fd92f925e8f462391c1e
Rudranayak17 <rudranayak311@gmail.com>
2024-11-11 15:28:13 +0530

    update the ui

--- src/page/Apply.jsx
+++ src/page/Apply.jsx
@@ -5,9 +5,9 @@ import Form from '../components/FormDetail';
 const Apply = () => {
   return (
     <div className="flex justify-center items-center p-6 gap-7 min-h-screen">
-  
+      <div className="w-full sm:w-3/5"> {/* Full width on mobile, 60% on larger screens */}
         <Form />
-  
+      </div>
     </div>
   );
 };
